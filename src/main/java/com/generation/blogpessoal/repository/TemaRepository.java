0c85e0bedfd34ad9bfb968d10cfaf1ced7c478e2
Gabriel Camargo <camargoxg@gmail.com>
2024-07-15 21:11:22 -0300

    :sparkles: feat: Construção package Service e classe UsuarioService

--- src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Tema;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 
 	/* SELECT * FROM tb_temass WHERE descricao LIKE "%descricao%" */
